1ac364746ce4d7fcbc0af40ed0b832aad256e4b9
Jakub <jakubp.jan@gmail.com>
2026-01-30 10:45:01 +0100

    firsst commit

---
+++ 1/src/app/app.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  title = 'Travel Planner';
+
+  steps = signal([
+    {
+      title: 'Choose a Destination',
+      text: 'Pick your dream location',
+      icon: '📍',
+    },
+    {
+      title: 'Plan Your Trip',
+      text: 'Create your custom itinerary',
+      icon: '🗺️',
+    },
+    {
+      title: 'Travel & Enjoy',
+      text: 'Have an unforgettable adventure',
+      icon: '✈️',
+    },
+  ]);
+}
